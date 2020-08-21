ab445a635dfe9d90403822661f35b0daf9dedc3e
deshi <deshi93@yandex.ru>
2020-08-21 21:14:38 +0300

    Add simple example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
+
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
 
 function App() {
+  const { register, handleSubmit, control, watch } = useForm<IFormInput>();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit((data) => {
+      console.log(data)
+    })}>
+      <input name="firstName" ref={register({ required: 'some text error', maxLength: 20 })} />
+      <input name="lastName" ref={register({ pattern: /^[A-Za-z]+$/i })} />
+      <input name="age" type="number" ref={register({ validate: (num) => num > 10 ? 'error mess' : true })} />
+
+      <input type="submit" />
+    </form>
   );
 }
 
